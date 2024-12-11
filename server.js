42d224704822d3976da24bef1cd9f87723b8b5e6
Zarpec <axelza25@gmail.com>
2024-12-10 18:45:06 -0600

    se crea el archivo server.js con la configuración del servidor y las rutas para categorías organizando los archivos en arquitectura MVC

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import categoriaRoutes from './src/routes/categoriaRoutes.js';
+
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const port = 3000;
+
+app.use('/categoria', categoriaRoutes);
+
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
