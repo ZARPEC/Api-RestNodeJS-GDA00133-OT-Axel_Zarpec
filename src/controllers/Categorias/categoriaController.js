f7b968db4869eae4402ba97fabf9675bf50e77bd
Zarpec <axelza25@gmail.com>
2024-12-13 17:30:49 -0600

    se agregan funciones para manejar subcategorías en el controlador y modelo, incluyendo agregar y mostrar subcategorías, mejorando la gestión de categorías en la arquitectura MVC

--- src/controllers/Categorias/categoriaController.js
+++ src/controllers/Categorias/categoriaController.js
@@ -1,13 +1,13 @@
-import {agregarCategoria,mostrarCategorias} from "../../models/categorias/categoriaModel.js";
-
+import {agregarCategoria,mostrarCategorias, agregarSubCategoria} from "../../models/categorias/categoriaModel.js";
+//categorias
 export async function AgregarCategoria(req, res) {
   try {
-    const { nombreCategoria } = req.body;
-    const result = await agregarCategoria(nombreCategoria);
-    res.status(200).json(result);
+    const nombreCat= req.body.nombre; //Se captura el nombre de la categoria desde la peticion post del cliente
+    const result = await agregarCategoria(nombreCat);//Se llama a la funcion agregarCategoria del modelo
+    res.status(200).json(result);//Se envia la respuesta al cliente
   } catch (err) {
-    console.log(err);
-    res.status(500).send("Error al agregar la categoria");
+    console.log(err); //En caso de error se muestra el error
+    res.status(500).send("Error al agregar la categoria"); //Se envia un mensaje de error al cliente
   }
 }
 
@@ -19,4 +19,34 @@ export async function mostrarCategoriasModel(req, res) {
     console.log(err);
     res.status(500).send("Error al mostrar las categorias");
   }
+}
+
+//subcategorias
+
+export async function AgregarSubCategoria(req, res) {
+  try {
+    const nombreSubCat = req.body.nombreSubCategoria;
+    const subcategoria_padre = req.body.subcategoria_padre;
+    const categoria_fk = req.body.categoria_fk;
+    const result = await agregarSubCategoria(nombreSubCat, subcategoria_padre, categoria_fk);
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error al agregar la subcategoria");
+  }
+}
+
+export async function MostrarSubCategorias(req, res) {
+  try {
+    const result = await mostrarSubCategorias();
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error al mostrar las subcategorias");
+  }
+}
+
+
+export function pruebaGet(req, res) {
+  res.send(req.query);
 }
--- src/models/categorias/categoriaModel.js
+++ src/models/categorias/categoriaModel.js
@@ -1,43 +1,56 @@
 import sql from "mssql";
 import dbConfig from "../../config/dbConfig.mjs";
 
-
-export async function agregarCategoria(nombreCategoria){
-    try {
-
-        await sql.connect(dbConfig);
-        const result = await sql.query`insert into dbo.Categoria_producto (nombre_categoria) values (${nombreCategoria})`;
-        return result.recordset;
-    }catch(err){
-
-        throw err;
-    }finally{
-        sql.close();
-    }
-}
-
-export async function agregarSubCategoria(nombreSubCategoria, subcategoria_padre, categoria_fk){
-    try {
-
-        await sql.connect(dbConfig);
-        await sql.query`insert into dbo.subcategoria (subcategoria, subcategoria_padre,categoria_fk) values (${nombreSubCategoria}, ${subcategoria_padre}, ${categoria_fk})`;
-    }catch(err){
-
-        throw err;
-    }finally{
-        sql.close();
-    }
+//categorias
+export async function agregarCategoria(nombreCategoria) {
+  try {
+    await sql.connect(dbConfig); //Se conecta a la base de datos
+    const result = await new sql.Request()
+      .input("Categoria", nombreCategoria)
+      .execute("spInsertar_Categoria"); //Se inserta la categoria en la base de datos
+    return result.recordset; //Se retorna el resultado
+    /*
+        const result = await sql.query`insert into dbo.Categoria_producto (nombre_categoria) values (${nombreCategoria})`;//Se inserta la categoria en la base de datos
+        return result.recordset; //Se retorna el resultado */
+  } catch (err) {
+    throw err; //en caso de error se lanza una excepcion mostrando el error
+    console.log(err);
+  } finally {
+    sql.close();
+  }
 }
 
 export async function mostrarCategorias() {
-    try{
-        await sql.connect(dbConfig);
-        const result = await sql.query`select * from dbo.Categoria_producto`;
-        return result.recordset;
-    }catch(err){
-        throw err;
-    }finally{
-        sql.close();
-    }
+  try {
+    await sql.connect(dbConfig);
+    const result = await sql.query`select * from dbo.Categoria_producto`;
+    return result.recordset;
+  } catch (err) {
+    throw err;
+  } finally {
+    sql.close();
+  }
 }
 
+//subcategorias
+export async function agregarSubCategoria(nombreSubCategoria,subcategoria_padre,categoria_fk)
+ {
+  try {
+    await sql.connect(dbConfig);
+    const result = await new sql.Request()
+      .input("subcategoria", nombreSubCategoria)
+      .input("subcategoria_padre", subcategoria_padre)
+      .input("categoria_fk", categoria_fk)
+      .execute("spInsertar_subcategoria");
+    return result.recordset;
+  } catch (err) {
+    throw err;
+    console.error(err);
+  } finally {
+    sql.close();
+  }
+}
+
+export async function mostrarSubCategorias(){
+    
+}
