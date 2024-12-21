f690297a7e24208afd728593c3aac3cb9d6d43b2
Zarpec <axelza25@gmail.com>
2024-12-20 22:24:06 -0600

    se agrega autenticación JWT a la ruta de agregar producto

--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import {AgregarProducto,MostrarProductos} from '../controllers/Productos/productoController.js';
+import authenticateToken from '../../src/middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post("/AgregarProducto", AgregarProducto);
+router.post("/AgregarProducto",authenticateToken, AgregarProducto);
 router.get("/mostrarProductos", MostrarProductos);
 export default router;
