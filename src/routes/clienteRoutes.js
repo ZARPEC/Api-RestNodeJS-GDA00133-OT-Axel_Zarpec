eea692c30dfb8ed2ad719624d2d101d3c22dc388
Zarpec <axelza25@gmail.com>
2024-12-20 22:23:58 -0600

    se agrega autenticación JWT a la ruta de agregar cliente

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {agregarCliente} from '../controllers/Clientes/clientesController.js';
+import authenticateToken from '../../src/middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post("/AgregarCliente", agregarCliente);
+router.post("/AgregarCliente",authenticateToken, agregarCliente);
+
 
 export default router;
