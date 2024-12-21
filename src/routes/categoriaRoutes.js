fe3d33a71f518932d027c6e9fca6500b2f948178
Zarpec <axelza25@gmail.com>
2024-12-20 22:22:37 -0600

    se agrega autenticación JWT a las rutas de agregar categoría y subcategoría

--- src/routes/categoriaRoutes.js
+++ src/routes/categoriaRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import {AgregarCategoria,mostrarCategoriasModel,AgregarSubCategoria,pruebaGet,MostrarSubCategorias} from '../controllers/Categorias/categoriaController.js';
+import authenticateToken from '../../src/middleware/authMiddleware.js';
 
 const router = express.Router();
 //categorias
-router.post("/AgregarCategoria", AgregarCategoria);
+router.post("/AgregarCategoria",authenticateToken, AgregarCategoria);
 router.get("/mostrarCategorias", mostrarCategoriasModel);
 
 //subcategorias
 
-router.post("/agregarSubCategoria", AgregarSubCategoria);
+router.post("/agregarSubCategoria",authenticateToken, AgregarSubCategoria);
 router.get("/mostrarSubCategorias", MostrarSubCategorias);
 
 router.get("/pruebaGet", pruebaGet);
