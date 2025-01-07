d4a2d7c33f67f30fc698a05222ff183cc3706dd1
Zarpec <axelza25@gmail.com>
2025-01-06 21:58:25 -0600

    se añade la ruta para mostrar clientes en el enrutador de clientes

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {agregarCliente, modificarCliente, modificarEstadoCliente} from '../controllers/Clientes/clientesController.js';
+import {agregarCliente, modificarCliente, modificarEstadoCliente, mostrarClientes} from '../controllers/Clientes/clientesController.js';
 import authenticateToken from '../../src/middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/AgregarCliente",authenticateToken, agregarCliente);
+router.get("/mostrarClientes",authenticateToken, mostrarClientes);
 router.post("/modificarCliente",authenticateToken, modificarCliente);
 router.post("/modificarEstadoCliente",authenticateToken, modificarEstadoCliente);
 
