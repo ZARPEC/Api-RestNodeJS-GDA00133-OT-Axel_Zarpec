8a7e293af2e3f0ebb2e1ecd42c234d2d335e3a2b
Zarpec <axelza25@gmail.com>
2025-01-16 06:03:12 -0600

    se refactorizan las funciones del modelo 'rol' para utilizar Sequelize, mejorando la consistencia y el manejo de errores

--- src/models/rolModel.js
+++ src/models/rolModel.js
@@ -2,45 +2,43 @@ import sql from "mssql";
 import sequelize from "../config/dbConfig.mjs";
 import rolesSequelize from "./sequelize/rol.js";
 
-export async function AgregarRolModel(rol){
-    try{
-        await sql.connect(dbConfig);
-        const result = await new sql.Request().
-        input("rol",rol).
-        execute("spInsertar_rol");
-        return result.recordset;
-    }catch(err){
-        throw err;
-        console.error(err);
-    }
-    finally{
-        sql.close();
-    }
+export async function AgregarRolModel(rol) {
+  try {
+    await sql.connect(dbConfig);
+    const result = await sequelize.query(`EXEC spInsertar_rol :rol`, {
+      replacements: {
+        rol: rol,
+      },
+      type: sequelize.QueryTypes.RAW,
+    });
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
-export async function mostrarRolesModel(){
-    try{
-        await sequelize.authenticate();
-        const roles = await rolesSequelize.findAll();
-        return roles;
-    }catch(err){
-        throw err;
-    }
+export async function mostrarRolesModel() {
+  try {
+    await sequelize.authenticate();
+    const roles = await rolesSequelize.findAll();
+    return roles;
+  } catch (err) {
+    throw err;
+  }
 }
 
-export async function modificarRolModel(idrol,rolNuevo){
-    try{
-        await sql.connect(dbConfig);
-        const result = await new sql.Request().
-        input('id',idrol).
-        input('nuevo',rolNuevo).
-        execute('spModificar_rol');
-        return result.recordset;
-    }catch(err){
-        throw err;
-        console.error(err);
-    }
-    finally{
-        sql.close();
-    }
+export async function modificarRolModel(idrol, rolNuevo) {
+  try {
+    await sql.connect(dbConfig);
+    const result = await sequelize.query(`EXEC spModificar_rol :id, :nuevo`, {
+      replacements: {
+        id: idrol,
+        nuevo: rolNuevo,
+      },
+      type: sequelize.QueryTypes.RAW,
+    });
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
